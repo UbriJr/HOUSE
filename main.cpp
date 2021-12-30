827ba562b04afda3a4daceb541d3be7138514d0c
UbriJr <eddieubrijr@yahoo.com>
2021-12-29 16:04:44 -0800

    updates to casino war game

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 //
 
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <fstream>
 #include <chrono>
 #include <thread>
@@ -20,9 +22,12 @@ public:
         RegisterOrLogin();
     }
     
-    // returns the number of tokens the player has (type string)
-    std::string getTokens(){
-        return Tokens;
+    // returns the number of tokens the player has
+    int getTokens(){
+        std::stringstream ss;
+        ss<<Tokens;
+        ss>>covertedTokens;
+        return covertedTokens;
     }
     
 private:
@@ -30,6 +35,7 @@ private:
     // the number of tokens the player has
     // this number comes from txt file.
     std::string Tokens;
+    int covertedTokens; 
     
     // prints a welcome banner and adds space to clear terminal window
     void Welcome(){
@@ -98,7 +104,7 @@ private:
         
         std::string pleaseWait = "PLEASE WAIT WHILE WE BRING YOU TO THE GAME FLOOR\n";
         
-        int WaitTime = 10000;
+        int WaitTime = 5000;
         
         bool InfoFound = false;
         
@@ -202,9 +208,9 @@ private:
     // welcomes player to the game floor
     void Welcome(){
         system("clear");
-        int TextTime = 125;
-        int ClearWaitTime = 1500;
-        std::string welcomeText = "WELCOME TO THE GAME FLOOR \nTHIS IS WHERE ALL THE GAMES ARE HELD \nHAVE FUN AND RELAX, BUT REMEMBER, THE HOUSE ALWAYS WINS.\n";
+        int TextTime = 125; // was 125
+        int ClearWaitTime = 3000; // was 3000
+        std::string welcomeText = "\n           WELCOME TO THE GAME FLOOR \nTHIS IS WHERE ALL THE GAMES ARE HELD \nHAVE FUN AND RELAX, BUT REMEMBER, THE HOUSE ALWAYS WINS.\n";
         unsigned long length = welcomeText.length();
         
         for(int i = 0; i < length; i++){
@@ -214,7 +220,24 @@ private:
         std::this_thread::sleep_for(std::chrono::milliseconds(ClearWaitTime));
         GameOptions();
     }
+
+    void GameOptions(){
+        system("clear");
+        bool isValid;
+        int userInput;
+        std::cout<<"                \nGAME ROOM\n";
+        do {
+            std::cout << "1: CASINO WAR \n";
+            std::cout << "2: GAME 2 \n";
+            std::cout << "3: GAME 3 \n";
+            std::cout << "SELECT AN OPTION: ";
+            std::cin >> userInput;
+            isValid = InputValidation(userInput);
+        } while (!isValid);
+        InputControlFlow(userInput);
+    }
     
+    // validates user selected a valid option (for a game only)
     bool InputValidation(int userInput){
         
         // clear console
@@ -237,51 +260,105 @@ private:
         }
     }
     
-    // direct input to either register or login method
+    // once a valid input is given game branches to the correct game
     void InputControlFlow(int userInput){
-        int GameOne = 1;
-        int GameTwo = 2;
-        int GameThree = 3;
+        int GameOneOption = 1;
+        int GameTwoOption = 2;
+        int GameThreeOption = 3;
         
-        if(userInput == (GameOne)){
-            std::cout<<"GameOne\n";
-            //GameOne();
-        } else if(userInput == (GameTwo)){
+        if(userInput == (GameOneOption)){
+            //std::cout<<"GameOne\n";
+            CasinoWarSelection();
+        } else if(userInput == (GameTwoOption)){
             std::cout<<"GameTwo\n";
             //GameTwo();
-        } else if(userInput == GameThree){
+        } else if(userInput == GameThreeOption){
             std::cout<<"GameThree\n";
             //GameThree();
         }
     }
-    
-    void GameOne(){
-        
-    }
-    
-    void GameTwo(){
-        
-    }
-    
-    void GameThree(){
-        
-    }
-    
-    void GameOptions(){
+    // prompts user to go to game rules or the actual game (for CasinoWar only)
+    void CasinoWarSelection(){
+        int instructions = 1;
+        int play = 2;
+        int userInput; 
+
         system("clear");
-        bool isValid;
-        int userInput;
-        std::cout<<"                GAME ROOM\n";
-        do {
-            std::cout << "1: GAME 1 \n";
-            std::cout << "2: GAME 2 \n";
-            std::cout << "3: GAME 3 \n";
-            std::cout << "SELECT AN OPTION: ";
-            std::cin >> userInput;
-            isValid = InputValidation(userInput);
-        } while (!isValid);
-        
-        InputControlFlow(userInput);
+        std::cout<<"\n                  CASINO WAR\n"<<std::endl;
+        std::cout<<"1: HOW TO PLAY CASINO WAR"<<std::endl;
+        std::cout<<"2: PLAY CASINO WAR"<<std::endl; 
+        std::cout<<"PLEASE SELECT AN OPTION: ";
+        std::cin>>userInput;
+
+        if(userInput == (instructions)){
+            CasinoWarInstructions();
+        }
+        else if(userInput == (play)){
+            CasinoWarPlay();
+        }
+    }
+
+    // the method that plays the casino war game
+    void CasinoWarPlay(){
+
+        system("clear");
+        IntroductionClass tokenObj;
+        int availableTokens = tokenObj.getTokens();
+        int wager; 
+        int playersCard;
+        std::string pCard;
+        int dealersCard;
+        std::string dCard;
+        std::string Show;
+
+        std::cout<<"\n                                  CASINO WAR\n";
+        std::cout<<"\n          PLEASE ENTER THE NUMBER OF TOKENS YOU WOULD LIKE TO WAGER: ";
+        std::cin>>wager;
+        std::cout<<"\n                              YOU WAGERED "<<wager<<" TOKENS \n";
+
+        playersCard = randomCard();
+        pCard = cardDealt(playersCard);
+
+        std::cout<<"\n                                  YOUR CARD: "<<pCard<<"\n";
+        std::cout<<"\nTYPE SHOW OR 1 WHEN READY TO SHOW DEALERS CARD: ";
+        std::cin>>Show;
+
+        dealersCard = randomCard();
+        std::cout<<"\n Dealers Card: "<<dealersCard<<std::endl;
+    }
+
+    // instructions for casino war
+    void CasinoWarInstructions(){
+        system("clear");
+        std::string instructMessage = "\n                                 CASINO WAR\n\n-THE PLAYER AND THE DEALER ARE EACH DEALT ONE CARD.\n-IF THE PLAYER'S CARD IS HIGHER THEN THE DEALER, HE WINS THE WAGER THEY BET.\n-OTHERWISE THE PLAYER LOSES.\n-A TIE OCCURS WHEN THE PLAYER AND THE DEALER EACH HAVE CARDS OF THE SAME RANK.\n-THE PLAYER AND DEALER ARE DEALT ANOTHER CARD TO DETERMINE THE WINNER.\n";
+        std::string cardRanks = "\n             CARD RANKS (IN ORDER FROM THE HIGHEST TO LOWEST):\n           ACE, KING, QUEEN, JACK, 10, 9, 8, 7, 6, 5, 4, 3, 2 \n";
+
+        std::cout<<instructMessage;
+        std::cout<<cardRanks<<std::endl;
+    }
+
+    int randomCard(){
+        srand((unsigned int) time(NULL));
+        int card = rand() % 13 + 1;
+        //int card = rand() % 4 + 11;
+        return card;
+    }
+
+    std::string cardDealt(int number){
+        std::string result;
+
+        if(number == (14)){
+            result = "ACE";
+        }else if(number == (13)){
+            result = "KING";
+        }else if(number == (12)){
+            result = "QUEEN";
+        }else if(number == (11)){
+            result = "JACK";
+        }else {
+            result = std::to_string(number);
+        }
+        return result; 
     }
 };
 
@@ -289,12 +366,12 @@ private:
 int main(int argc, const char * argv[]) {
     
     // Introduction section (register, login, transfer)
-    IntroductionClass IntroObj;
-    IntroObj.RunIntroduction();
+    //IntroductionClass IntroObj;
+    //IntroObj.RunIntroduction();
     
     // Game section (games)
-    //GameRoom roomObj;
-    //roomObj.RunGameRoom();
+    GameRoom roomObj;
+    roomObj.RunGameRoom();
     
     //return 0;
 }
